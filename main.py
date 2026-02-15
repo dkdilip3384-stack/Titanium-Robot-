472d747250c4862b78615b425692cc1fd967335a
dkdilip3384-stack <dkdilip3384@gmail.com>
2026-02-16 00:21:44 +0530

    MASTER BOND - REPAIR ATTEMPT 185

--- main.py
+++ main.py
@@ -1,56 +1,48 @@
 ```python
-import os
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.clock import Clock
 from kivy.utils import platform
+from kivy.core.window import Window
 
-if platform == 'android':
-    from jnius import autoclass, cast
-    from android.runnable import run_on_ui_thread
-    
-    WebView = autoclass('android.webkit.WebView')
-    WebViewClient = autoclass('android.webkit.WebViewClient')
-    LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
-    LinearLayout = autoclass('android.widget.LinearLayout')
-    Activity = autoclass('org.kivy.android.PythonActivity').mActivity
-else:
-    run_on_ui_thread = lambda x: x
-
-class WebViewWidget(BoxLayout):
-    def __init__(self, **kwargs):
-        super(WebViewWidget, self).__init__(**kwargs)
-        self.url = "https://delivery-tracking-delta.vercel.app/"
-        if platform == 'android':
-            self.create_webview()
-        else:
-            from kivy.uix.label import Label
-            self.add_widget(Label(text="WebView only supported on Android.\nURL: " + self.url))
-
-    @run_on_ui_thread
-    def create_webview(self):
-        self.webview = WebView(Activity)
-        self.webview.getSettings().setJavaScriptEnabled(True)
-        self.webview.getSettings().setDomStorageEnabled(True)
-        self.webview.getSettings().setAllowFileAccess(True)
-        self.webview.setWebViewClient(WebViewClient())
-        
-        layout = LinearLayout(Activity)
-        layout.addView(self.webview)
-        
-        Activity.setContentView(layout)
-        self.webview.loadUrl(self.url)
-
-class MainApp(App):
+class DeliveryTrackerApp(App):
     def build(self):
-        return WebViewWidget()
+        # Setting a background color to prevent SDL2 black screen issues
+        Window.clearcolor = (1, 1, 1, 1)
+        return None
 
-    def on_pause(self):
-        return True
+    def on_start(self):
+        if platform == 'android':
+            from jnius import autoclass
+            from android.runnable import run_on_ui_thread
 
-    def on_resume(self):
-        pass
+            # Android classes
+            WebView = autoclass('android.webkit.WebView')
+            WebViewClient = autoclass('android.webkit.WebViewClient')
+            Activity = autoclass('org.kivy.android.PythonActivity').mActivity
+
+            @run_on_ui_thread
+            def setup_webview():
+                # Initialize WebView
+                webview = WebView(Activity)
+                settings = webview.getSettings()
+                
+                # Critical settings for modern web apps (Vercel/React/etc)
+                settings.setJavaScriptEnabled(True)
+                settings.setDomStorageEnabled(True)
+                settings.setAllowFileAccess(True)
+                settings.setLoadsImagesAutomatically(True)
+                settings.setDatabaseEnabled(True)
+                
+                # Prevent opening links in external browser
+                webview.setWebViewClient(WebViewClient())
+                
+                # Load URL
+                webview.loadUrl("https://delivery-tracking-delta.vercel.app/")
+                
+                # Overlay the WebView onto the Kivy Activity
+                Activity.setContentView(webview)
+
+            setup_webview()
 
 if __name__ == '__main__':
-    MainApp().run()
+    DeliveryTrackerApp().run()
 ```
