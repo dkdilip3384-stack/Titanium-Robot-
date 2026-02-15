79ab6ccbd6ee2558cebcd452982722f7869bf5ca
dkdilip3384-stack <dkdilip3384@gmail.com>
2026-02-15 20:26:25 +0530

    Supreme: Final Core Push

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 from android.runnable import run_on_ui_thread
 from jnius import autoclass, PythonJavaClass
 from kivy.core.window import Window
 from android.permissions import request_permissions, Permission
+import requests
 
 WebView = autoclass('android.webkit.WebView')
 WebViewClient = autoclass('android.webkit.WebViewClient')
 WebChromeClient = autoclass('android.webkit.WebChromeClient')
 activity = autoclass('org.kivy.android.PythonActivity').mActivity
 
-class TitaniumV45Stable(App):
+class TitaniumSupreme(MDApp):
     def build(self):
+        self.theme_cls.primary_palette = "BlueGray"
         Window.bind(on_keyboard=self.on_back_button)
         request_permissions([Permission.INTERNET, Permission.ACCESS_FINE_LOCATION, Permission.CAMERA])
         Clock.schedule_once(self.init_webview, 1)
@@ -27,14 +30,14 @@ class TitaniumV45Stable(App):
         s.setGeolocationEnabled(True)
         s.setMixedContentMode(0) 
         
-        class StableClient(PythonJavaClass):
+        class SupremeClient(PythonJavaClass):
             __javainterfaces__ = ['android/webkit/WebViewClient']
             __javacontext__ = 'app'
             def shouldOverrideUrlLoading(self, view, url):
                 view.loadUrl(url)
                 return True
                 
-        self.webview.setWebViewClient(StableClient())
+        self.webview.setWebViewClient(SupremeClient())
         self.webview.setWebChromeClient(WebChromeClient())
         self.webview.loadUrl('https://delivery-tracking-delta.vercel.app/')
         activity.setContentView(self.webview)
@@ -46,4 +49,4 @@ class TitaniumV45Stable(App):
         return False
 
 if __name__ == '__main__':
-    TitaniumV45Stable().run()
+    TitaniumSupreme().run()
