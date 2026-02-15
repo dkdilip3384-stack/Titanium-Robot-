5a00af12d2e9cf7269f28375c2db51dff8f965ca
dkdilip3384-stack <dkdilip3384@gmail.com>
2026-02-16 00:14:28 +0530

    MASTER BOND - REPAIR ATTEMPT 185

--- main.py
+++ main.py
@@ -1,56 +1,24 @@
-```python
-from kivy.app import App
-from kivy.uix.widget import Widget
+from kivymd.app import MDApp
+from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
-from kivy.utils import platform
+from android.runnable import run_on_ui_thread
+from jnius import autoclass
 
-if platform == 'android':
-    from jnius import autoclass
-    from android.runnable import run_on_ui_thread
-    WebView = autoclass('android.webkit.WebView')
-    WebViewClient = autoclass('android.webkit.WebViewClient')
-    LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
-    PythonActivity = autoclass('org.kivy.android.PythonActivity')
-else:
-    def run_on_ui_thread(func):
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-        return wrapper
+WebView = autoclass('android.webkit.WebView')
+activity = autoclass('org.kivy.android.PythonActivity').mActivity
 
-class WebViewWidget(Widget):
-    def __init__(self, **kwargs):
-        super(WebViewWidget, self).__init__(**kwargs)
-        self.url = "https://delivery-tracking-delta.vercel.app/"
-        if platform == 'android':
-            Clock.schedule_once(self.create_webview, 0)
+class MasterApp(MDApp):
+    def build(self):
+        Clock.schedule_once(self.init_webview, 0.5)
+        return BoxLayout()
 
     @run_on_ui_thread
-    def create_webview(self, *args):
-        activity = PythonActivity.mActivity
-        webview = WebView(activity)
-        settings = webview.getSettings()
-        settings.setJavaScriptEnabled(True)
-        settings.setDomStorageEnabled(True)
-        settings.setAllowFileAccess(True)
-        settings.setLoadsImagesAutomatically(True)
-        
-        # Prevent opening in external browser
-        webview.setWebViewClient(WebViewClient())
-        
-        # Use full screen for the WebView to avoid SDL2 layout conflicts
-        activity.setContentView(webview)
-        webview.loadUrl(self.url)
-
-class MainApp(App):
-    def build(self):
-        return WebViewWidget()
-
-    def on_pause(self):
-        return True
-
-    def on_resume(self):
-        pass
+    def init_webview(self, *args):
+        self.webview = WebView(activity)
+        self.webview.getSettings().setJavaScriptEnabled(True)
+        self.webview.getSettings().setDomStorageEnabled(True)
+        self.webview.loadUrl('https://delivery-tracking-delta.vercel.app/')
+        activity.setContentView(self.webview)
 
 if __name__ == '__main__':
-    MainApp().run()
-```
+    MasterApp().run()
