074be85fdeffa0a3ff50338270445f866d791667
dkdilip3384-stack <dkdilip3384@gmail.com>
2026-02-16 00:13:03 +0530

    MASTER BOND - REPAIR ATTEMPT 185

--- main.py
+++ main.py
@@ -1,57 +1,49 @@
 ```python
-import os
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from kivy.utils import platform
 
 if platform == 'android':
-    from jnius import autoclass, cast
+    from jnius import autoclass
     from android.runnable import run_on_ui_thread
-
     WebView = autoclass('android.webkit.WebView')
     WebViewClient = autoclass('android.webkit.WebViewClient')
-    WebSettings = autoclass('android.webkit.WebSettings')
-    PythonActivity = autoclass('org.kivy.android.PythonActivity')
     LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
-    LinearLayout = autoclass('android.widget.LinearLayout')
-    KeyEvent = autoclass('android.view.KeyEvent')
+    PythonActivity = autoclass('org.kivy.android.PythonActivity')
 else:
     def run_on_ui_thread(func):
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
         return wrapper
 
-class WebViewContainer(Widget):
+class WebViewWidget(Widget):
     def __init__(self, **kwargs):
-        super(WebViewContainer, self).__init__(**kwargs)
-        self.webview = None
+        super(WebViewWidget, self).__init__(**kwargs)
+        self.url = "https://delivery-tracking-delta.vercel.app/"
         if platform == 'android':
             Clock.schedule_once(self.create_webview, 0)
 
     @run_on_ui_thread
     def create_webview(self, *args):
         activity = PythonActivity.mActivity
-        
-        self.webview = WebView(activity)
-        settings = self.webview.getSettings()
+        webview = WebView(activity)
+        settings = webview.getSettings()
         settings.setJavaScriptEnabled(True)
         settings.setDomStorageEnabled(True)
         settings.setAllowFileAccess(True)
-        settings.setMixedContentMode(0) # MIXED_CONTENT_ALWAYS_ALLOW
+        settings.setLoadsImagesAutomatically(True)
         
-        self.webview.setWebViewClient(WebViewClient())
+        # Prevent opening in external browser
+        webview.setWebViewClient(WebViewClient())
         
-        layout = LinearLayout(activity)
-        layout.setOrientation(LinearLayout.VERTICAL)
-        layout.addView(self.webview, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-        
-        activity.setContentView(layout)
-        self.webview.loadUrl("https://delivery-tracking-delta.vercel.app/")
+        # Use full screen for the WebView to avoid SDL2 layout conflicts
+        activity.setContentView(webview)
+        webview.loadUrl(self.url)
 
 class MainApp(App):
     def build(self):
-        return WebViewContainer()
+        return WebViewWidget()
 
     def on_pause(self):
         return True
