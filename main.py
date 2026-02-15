98e58e7be47d0b402aaaee94f0ad91335340d417
dkdilip3384-stack <dkdilip3384@gmail.com>
2026-02-16 00:21:15 +0530

    MASTER BOND - REPAIR ATTEMPT 185

--- main.py
+++ main.py
@@ -1,44 +1,49 @@
 ```python
+import os
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.utils import platform
-from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
+from kivy.utils import platform
 
-class WebViewApp(App):
-    def build(self):
+if platform == 'android':
+    from jnius import autoclass, cast
+    from android.runnable import run_on_ui_thread
+    
+    WebView = autoclass('android.webkit.WebView')
+    WebViewClient = autoclass('android.webkit.WebViewClient')
+    LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
+    LinearLayout = autoclass('android.widget.LinearLayout')
+    Activity = autoclass('org.kivy.android.PythonActivity').mActivity
+else:
+    run_on_ui_thread = lambda x: x
+
+class WebViewWidget(BoxLayout):
+    def __init__(self, **kwargs):
+        super(WebViewWidget, self).__init__(**kwargs)
         self.url = "https://delivery-tracking-delta.vercel.app/"
         if platform == 'android':
-            from android.runnable import run_on_ui_thread
-            self.init_webview()
-        return Widget()
+            self.create_webview()
+        else:
+            from kivy.uix.label import Label
+            self.add_widget(Label(text="WebView only supported on Android.\nURL: " + self.url))
 
     @run_on_ui_thread
-    def init_webview(self):
-        from jnius import autoclass
-        
-        # Accessing Android System Classes
-        PythonActivity = autoclass('org.kivy.android.PythonActivity')
-        WebView = autoclass('android.webkit.WebView')
-        WebViewClient = autoclass('android.webkit.WebViewClient')
-        LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
-        LinearLayout = autoclass('android.widget.LinearLayout')
-        
-        activity = PythonActivity.mActivity
-        
-        # Initialize WebView
-        self.webview = WebView(activity)
+    def create_webview(self):
+        self.webview = WebView(Activity)
         self.webview.getSettings().setJavaScriptEnabled(True)
         self.webview.getSettings().setDomStorageEnabled(True)
-        self.webview.getSettings().setUserAgentString("Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.101 Mobile Safari/537.36")
+        self.webview.getSettings().setAllowFileAccess(True)
         self.webview.setWebViewClient(WebViewClient())
         
-        # Prevent SDL2 from conflicting with WebView Layout
-        layout = LinearLayout(activity)
-        layout.addView(self.webview, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-        activity.setContentView(layout)
+        layout = LinearLayout(Activity)
+        layout.addView(self.webview)
         
+        Activity.setContentView(layout)
         self.webview.loadUrl(self.url)
+
+class MainApp(App):
+    def build(self):
+        return WebViewWidget()
 
     def on_pause(self):
         return True
@@ -47,5 +52,5 @@ class WebViewApp(App):
         pass
 
 if __name__ == '__main__':
-    WebViewApp().run()
+    MainApp().run()
 ```
