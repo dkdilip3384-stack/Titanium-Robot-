26086d0a080524553383e3c67061813fab64235b
dkdilip3384-stack <dkdilip3384@gmail.com>
2026-02-15 23:40:28 +0530

    AI AUTO-FIX - Attempt 1

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 from android.runnable import run_on_ui_thread
 from jnius import autoclass, PythonJavaClass
-from android.permissions import request_permissions, Permission
 
 WebView = autoclass('android.webkit.WebView')
 activity = autoclass('org.kivy.android.PythonActivity').mActivity
 
-class RobotApp(MDApp):
+class TitaniumAI(MDApp):
     def build(self):
-        request_permissions([Permission.INTERNET])
         Clock.schedule_once(self.init_webview, 0.5)
         return BoxLayout()
 
@@ -18,9 +16,8 @@ class RobotApp(MDApp):
     def init_webview(self, *args):
         self.webview = WebView(activity)
         self.webview.getSettings().setJavaScriptEnabled(True)
-        self.webview.getSettings().setDomStorageEnabled(True)
         self.webview.loadUrl('https://delivery-tracking-delta.vercel.app/')
         activity.setContentView(self.webview)
 
 if __name__ == '__main__':
-    RobotApp().run()
+    TitaniumAI().run()
