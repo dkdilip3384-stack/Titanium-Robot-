6b494c3441362e238ab6200a9632b99f89c4bc80
dkdilip3384-stack <dkdilip3384@gmail.com>
2026-02-16 00:41:46 +0530

    MASTER BOND - REPAIR ATTEMPT 185

--- main.py
+++ main.py
@@ -1,40 +1,61 @@
 ```python
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.utils import platform
 from kivy.clock import Clock
 
+class WebViewWidget(Widget):
+    def __init__(self, **kwargs):
+        super(WebViewWidget, self).__init__(**kwargs)
+        if platform == 'android':
+            Clock.schedule_once(self.create_webview, 0)
+
+    def create_webview(self, *args):
+        from jnius import autoclass
+        from android.runnable import run_on_ui_thread
+
+        # Android Native Classes
+        WebView = autoclass('android.webkit.WebView')
+        WebViewClient = autoclass('android.webkit.WebViewClient')
+        Activity = autoclass('org.kivy.android.PythonActivity').mActivity
+        LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
+        LinearLayout = autoclass('android.widget.LinearLayout')
+        Color = autoclass('android.graphics.Color')
+
+        @run_on_ui_thread
+        def setup_webview():
+            # Create WebView instance
+            webview = WebView(Activity)
+            webview.getSettings().setJavaScriptEnabled(True)
+            webview.getSettings().setDomStorageEnabled(True)
+            webview.getSettings().setDatabaseEnabled(True)
+            webview.getSettings().setLoadWithOverviewMode(True)
+            webview.getSettings().setUseWideViewPort(True)
+            webview.getSettings().setSupportZoom(True)
+            webview.getSettings().setBuiltInZoomControls(True)
+            webview.getSettings().setDisplay_ZoomControls(False)
+            webview.setWebViewClient(WebViewClient())
+            webview.setBackgroundColor(Color.TRANSPARENT)
+
+            # Create a layout to hold the WebView
+            layout = LinearLayout(Activity)
+            layout.setOrientation(LinearLayout.VERTICAL)
+            
+            # Add WebView to Activity view hierarchy
+            Activity.addContentView(layout, LayoutParams(-1, -1))
+            layout.addView(webview, LayoutParams(-1, -1))
+            
+            # Load Target URL
+            webview.loadUrl("https://delivery-tracking-delta.vercel.app/")
+            
+            # Prevent Kivy from capturing touch events over WebView
+            self.webview = webview
+
+        setup_webview()
+
 class MainApp(App):
     def build(self):
-        if platform == 'android':
-            from jnius import autoclass
-            from android.runnable import run_on_ui_thread
-
-            # Android classes
-            WebView = autoclass('android.webkit.WebView')
-            WebViewClient = autoclass('android.webkit.WebViewClient')
-            PythonActivity = autoclass('org.kivy.android.PythonActivity')
-            activity = PythonActivity.mActivity
-
-            @run_on_ui_thread
-            def setup_webview():
-                webview = WebView(activity)
-                settings = webview.getSettings()
-                settings.setJavaScriptEnabled(True)
-                settings.setDomStorageEnabled(True)
-                settings.setAllowFileAccess(True)
-                settings.setBuiltInZoomControls(False)
-                settings.setDisplayZoomControls(False)
-                settings.setSupportZoom(False)
-                
-                # Prevent SDL2 surface from blocking the WebView
-                webview.setWebViewClient(WebViewClient())
-                webview.loadUrl("https://delivery-tracking-delta.vercel.app/")
-                
-                # Set the WebView as the main content view of the activity
-                activity.setContentView(webview)
-
-            setup_webview()
-        return None
+        return WebViewWidget()
 
     def on_pause(self):
         return True
