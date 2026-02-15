fd61ef49c7e990a809c4f7d53ae741815d9a3a10
dkdilip3384-stack <dkdilip3384@gmail.com>
2026-02-16 00:14:57 +0530

    MASTER BOND - REPAIR ATTEMPT 185

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-from kivymd.app import MDApp
-from kivy.uix.boxlayout import BoxLayout
+```python
+from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.clock import Clock
-from android.runnable import run_on_ui_thread
 from jnius import autoclass
+from android.runnable import run_on_ui_thread
 
+# Android classes
 WebView = autoclass('android.webkit.WebView')
-activity = autoclass('org.kivy.android.PythonActivity').mActivity
+WebViewClient = autoclass('android.webkit.WebViewClient')
+PythonActivity = autoclass('org.kivy.android.PythonActivity')
 
-class MasterApp(MDApp):
-    def build(self):
-        Clock.schedule_once(self.init_webview, 0.5)
-        return BoxLayout()
+class WebViewWidget(Widget):
+    def __init__(self, **kwargs):
+        super(WebViewWidget, self).__init__(**kwargs)
+        self.url = "https://delivery-tracking-delta.vercel.app/"
+        self.create_webview()
 
     @run_on_ui_thread
-    def init_webview(self, *args):
-        self.webview = WebView(activity)
-        self.webview.getSettings().setJavaScriptEnabled(True)
-        self.webview.getSettings().setDomStorageEnabled(True)
-        self.webview.loadUrl('https://delivery-tracking-delta.vercel.app/')
-        activity.setContentView(self.webview)
+    def create_webview(self):
+        activity = PythonActivity.mActivity
+        webview = WebView(activity)
+        webview.getSettings().setJavaScriptEnabled(True)
+        webview.getSettings().setDomStorageEnabled(True)
+        webview.getSettings().setAllowFileAccess(True)
+        webview.setWebViewClient(WebViewClient())
+        activity.setContentView(webview)
+        webview.loadUrl(self.url)
+
+class MainApp(App):
+    def build(self):
+        return WebViewWidget()
 
 if __name__ == '__main__':
-    MasterApp().run()
+    MainApp().run()
+```
