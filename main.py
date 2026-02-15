254f54b9e5f705299262fe027191b164a3f0c60e
dkdilip3384-stack <dkdilip3384@gmail.com>
2026-02-15 23:24:05 +0530

    V1000 ROBOT - Attempt 143

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 from android.runnable import run_on_ui_thread
 from jnius import autoclass, PythonJavaClass
-from kivy.core.window import Window
 from android.permissions import request_permissions, Permission
 
 WebView = autoclass('android.webkit.WebView')
 activity = autoclass('org.kivy.android.PythonActivity').mActivity
 
-class TitaniumInfinity(MDApp):
+class RobotApp(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "DeepPurple"
-        Window.bind(on_keyboard=self.on_back_button)
         request_permissions([Permission.INTERNET])
         Clock.schedule_once(self.init_webview, 0.5)
         return BoxLayout()
@@ -21,26 +17,10 @@ class TitaniumInfinity(MDApp):
     @run_on_ui_thread
     def init_webview(self, *args):
         self.webview = WebView(activity)
-        s = self.webview.getSettings()
-        s.setJavaScriptEnabled(True)
-        s.setDomStorageEnabled(True)
-        
-        class C(PythonJavaClass):
-            __javainterfaces__ = ['android/webkit/WebViewClient']
-            __javacontext__ = 'app'
-            def shouldOverrideUrlLoading(self, view, url):
-                view.loadUrl(url)
-                return True
-                
-        self.webview.setWebViewClient(C())
+        self.webview.getSettings().setJavaScriptEnabled(True)
+        self.webview.getSettings().setDomStorageEnabled(True)
         self.webview.loadUrl('https://delivery-tracking-delta.vercel.app/')
         activity.setContentView(self.webview)
 
-    def on_back_button(self, window, key, *args):
-        if key == 27 and hasattr(self, 'webview') and self.webview.canGoBack():
-            self.webview.goBack()
-            return True
-        return False
-
 if __name__ == '__main__':
-    TitaniumInfinity().run()
+    RobotApp().run()
